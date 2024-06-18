aa90b7ae74c9bbfb7cff94c3d4f173f128f93b96
StelllarAdmin <itmanager@stellartechpdkt.org>
2024-06-18 11:27:53 +0530

    Add project files.

---
+++ StellarBillingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StellarBillingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
