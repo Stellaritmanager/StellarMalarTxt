faf380137fae2910ccc035444bb8b83075ccca15
admin <itmanager@stellartechpdkt.org>
2024-07-22 17:28:36 +0530

    Rajalingam-Added Dashboard

--- StellarBillingSystem/Controllers/HomeController.cs
+++ StellarBillingSystem/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using StellarBillingSystem.Models;
 
 namespace StellarBillingSystem.Controllers;
@@ -11,13 +12,74 @@ public class HomeController : Controller
     
     public IActionResult Index()
     {
+
+        string salesMessage = GetSalesComparison();
+        decimal dailySales = GetDailySales();
+        decimal dailyPayments = GetDailyPayments();
+
+        ViewBag.SalesMessage = salesMessage;
+        ViewBag.DailySales = dailySales.ToString("F2"); 
+        ViewBag.DailyPayments = dailyPayments.ToString("F2");
         return View();
     }
 
-  
-    public IActionResult Privacy()
+    private string GetSalesComparison()
+    {
+        string result = string.Empty;
+
+        using (SqlConnection conn = new SqlConnection("Data Source=DESKTOP-L8EIGER\\SQLEXPRESS;Initial Catalog=StellarBilling;Integrated Security=True;Trust Server Certificate=True;"))
+        {
+            conn.Open();
+            using (SqlCommand cmd = new SqlCommand("SELECT dbo.CompareDailySales()", conn))
+            {
+                result = cmd.ExecuteScalar().ToString();
+            }
+        }
+
+        return result;
+    }
+
+    private decimal GetDailySales()
+    {
+        decimal result = 0;
+
+        using (SqlConnection conn = new SqlConnection("Data Source=DESKTOP-L8EIGER\\SQLEXPRESS;Initial Catalog=StellarBilling;Integrated Security=True;Trust Server Certificate=True;"))
+        {
+            conn.Open();
+            using (SqlCommand cmd = new SqlCommand("SELECT dbo.GetDailySales()", conn))
+            {
+                result = (decimal)cmd.ExecuteScalar();
+            }
+        }
+
+        return result;
+    }
+
+    private decimal GetDailyPayments()
+    {
+        decimal result = 0;
+
+        using (SqlConnection conn = new SqlConnection("Data Source=DESKTOP-L8EIGER\\SQLEXPRESS;Initial Catalog=StellarBilling;Integrated Security=True;Trust Server Certificate=True;"))
+        {
+            conn.Open();
+            using (SqlCommand cmd = new SqlCommand("SELECT dbo.GetDailyPayments()", conn))
+            {
+                result = (decimal)cmd.ExecuteScalar();
+            }
+        }
+
+        return result;
+    }
+
+
+        public IActionResult Privacy()
     {
         return View();
+    }
+    public IActionResult RedirectToReports()
+    {
+       
+        return RedirectToAction("Reports", "Reports");
     }
 
 }
