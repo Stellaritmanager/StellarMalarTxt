1c5acfefabce2daf7f23480abb5767a26ecf7463
admin <itmanager@stellartechpdkt.org>
2024-07-30 15:02:29 +0530

    Rajalingam

--- StellarBillingSystem/Controllers/LoginAuthenticationController.cs
+++ StellarBillingSystem/Controllers/LoginAuthenticationController.cs
@@ -33,10 +33,9 @@ namespace StellarBillingSystem.Controllers
         public IActionResult Administration()
         {
 
-            Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate"; // HTTP 1.1.
-            Response.Headers["Pragma"] = "no-cache"; // HTTP 1.0.
+            Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+            Response.Headers["Pragma"] = "no-cache";
             Response.Headers["Expires"] = "0";
-
 
             BusinessClassBilling Busbill = new BusinessClassBilling(_billingContext);
           
