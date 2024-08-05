a067a9fe063828ec4d12ae456b334a53a06a21ad
admin <itmanager@stellartechpdkt.org>
2024-08-05 14:40:07 +0530

    Yuvarani

---
+++ StellarBillingSystem/Migrations/20240805071855_initial69.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StellarBillingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial69 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImeiNumber",
+                table: "SHProductMaster",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SerialNumber",
+                table: "SHProductMaster",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImeiNumber",
+                table: "SHProductMaster");
+
+            migrationBuilder.DropColumn(
+                name: "SerialNumber",
+                table: "SHProductMaster");
+        }
+    }
+}
