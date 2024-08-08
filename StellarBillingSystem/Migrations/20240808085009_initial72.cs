c1a1de245bf72cdfa84d261d969ad7c38d3448a1
admin <itmanager@stellartechpdkt.org>
2024-08-08 14:22:38 +0530

    Rajalingam-Added Migration

---
+++ StellarBillingSystem/Migrations/20240808085009_initial72.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StellarBillingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial72 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BillTemplate",
+                table: "SHBranchMaster",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BillTemplate",
+                table: "SHBranchMaster");
+        }
+    }
+}
